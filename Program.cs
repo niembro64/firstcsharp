4c9ea4e1ab121e128b13c48ea25b61bdd0ef3ecd
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 13:12:00 -0400

    done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace firstcsharp
     {
         static void Main(string[] args)
         {
+
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(2, 8));
+
             Console.WriteLine("Hello Eric!");
 
 
